efb4e6729ccba7956d7062823c272292c1bdd4e1
liuxuesong930721 <liuxuesong930721@163.com>
2018-10-15 14:56:08 +0800

    local_json

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-var config = {"routers":"http://192.168.4.69/user/route/client_route","notification":"test.php","language":"Chin","version":"1.100"};
+var config = {"routers":"http://192.168.4.69:666/user/route/client_route","notification":"test.php","language":"Chin","version":"1.100"};
 module.exports = config;
