7e3f07b93002b049aa930a7308314d53c1a6cd84
liuxuesong930721 <liuxuesong930721@163.com>
2018-12-29 12:13:10 +0800

    class

--- src/views/components/watch/TravelExpense.js
+++ src/views/components/watch/TravelExpense.js
@@ -8,21 +8,21 @@
         render(){
             const {message} = this.props;
             return (
-                <div id="travel_exp_box" class="travel_exp_box">
-                <div class="tvl_row1">
+                <div id="travel_exp_box" className="travel_exp_box">
+                <div className="tvl_row1">
                     <span>部门</span>
                     <span>市场部</span>
                     <dt>差旅费报销单</dt>
                 </div>
-                <div class="tv_idcode">
+                <div className="tv_idcode">
                     <span>项目编号:</span>
                     <span>454564</span>
                 </div>
-                <div class="tvl_id_num">
+                <div className="tvl_id_num">
                     <span>差旅费报销单编号：</span>
                     <span></span>
                 </div>
-                <div class="tvl_row2">
+                <div className="tvl_row2">
                     <span>2018</span>
                     <i>年</i>
                     <span>12</span>
@@ -30,23 +30,24 @@
                     <span>28</span>
                     <i>日</i>
                 </div>
-                <div class="tvl_content1">
-                    <div class="tvl_row3_top1">
+                <div className="tvl_content1">
+                    <div className="tvl_row3_top1">
                         <span>出差人</span>
                         <span>刘雪松</span>
                         <span> 出差事由</span>
                         <span>项目经理</span>
                     </div>
-                    <table class="tvl_row3">
-                        <tr class="tvl_row3_tr1">
-                            <td colspan="4"><span>出</span>发</td>
-                            <td colspan="4">到达</td>
-                            <td rowspan="2">交通工具</td>
-                            <td rowspan="2">交通费</td>
-                            <td colspan="2">出差津贴</td>
-                            <td colspan="2"><span>其</span><span>他</span><span>费</span>用</td>
+                    <table className="tvl_row3">
+                        <tbody>
+                        <tr className="tvl_row3_tr1">
+                            <td colSpan="4"><span>出</span>发</td>
+                            <td colSpan="4">到达</td>
+                            <td rowSpan="2">交通工具</td>
+                            <td rowSpan="2">交通费</td>
+                            <td colSpan="2">出差津贴</td>
+                            <td colSpan="2"><span>其</span><span>他</span><span>费</span>用</td>
                         </tr>
-                        <tr class="tvl_row3_tr2">
+                        <tr className="tvl_row3_tr2">
                             <td>月</td>
                             <td>日</td>
                             <td>时</td>
@@ -60,7 +61,7 @@
                             <td><span>项</span>目</td>
                             <td>金额</td>
                         </tr>
-                        <tr class="tvl_row3_tr3">
+                        <tr className="tvl_row3_tr3">
                             <td>12</td>
                             <td>28</td>
                             <td>11</td>
@@ -76,7 +77,7 @@
                             <td><span>住</span><span>宿</span>费</td>
                             <td></td>
                         </tr>
-                        <tr class="tvl_row3_tr4">
+                        <tr className="tvl_row3_tr4">
                             <td></td>
                             <td></td>
                             <td></td>
@@ -92,7 +93,7 @@
                             <td>市内车费</td>
                             <td></td>
                         </tr>
-                        <tr class="tvl_row3_tr5">
+                        <tr className="tvl_row3_tr5">
                             <td></td>
                             <td></td>
                             <td></td>
@@ -108,7 +109,7 @@
                             <td>办公用品费</td>
                             <td></td>
                         </tr>
-                        <tr class="tvl_row3_tr6">
+                        <tr className="tvl_row3_tr6">
                             <td></td>
                             <td></td>
                             <td></td>
@@ -124,8 +125,8 @@
                             <td><span>其</span>他</td>
                             <td></td>
                         </tr>
-                        <tr class="tvl_row3_tr7">
-                            <td colspan="8"><span>合计</span><span></span></td>
+                        <tr className="tvl_row3_tr7">
+                            <td colSpan="8"><span>合计</span><span></span></td>
                             <td></td>
                             <td></td>
                             <td></td>
@@ -133,16 +134,17 @@
                             <td></td>
                             <td></td>
                         </tr>
+                        </tbody>
                     </table>
-                    <div class="tvl_row4">
-                        <div class="tvl_row4_box1">
+                    <div className="tvl_row4">
+                        <div className="tvl_row4_box1">
                             <span>报销总额</span>
                             <span><b>人民币</b><b>（大写）</b>
         
                             </span>
                             <dd>伍佰元整</dd>
                         </div>
-                        <div class="tvl_row4_box2">
+                        <div className="tvl_row4_box2">
                             <span>预借旅费</span>
                             <span>
                                 <dd>￥</dd>
@@ -150,7 +152,7 @@
                             </span>
                             <span></span>
                         </div>
-                        <div class="tvl_row4_box3">
+                        <div className="tvl_row4_box3">
                             <p>
                                 <span>补退金额</span>
                                 <span>￥</span>
@@ -163,7 +165,7 @@
                             </p>
                         </div>
                     </div>
-                    <div class="tvl_row5">
+                    <div className="tvl_row5">
                         <span>领款人</span>
                         <span></span>
                         <span>部门经理</span>
@@ -172,7 +174,7 @@
                         <span></span>
         
                     </div>
-                    <div class="tvl_row6">
+                    <div className="tvl_row6">
                         <span>执行总经理</span>
                         <span></span>
                         <span>总经理</span>
@@ -180,9 +182,9 @@
                     </div>
                 </div>
         
-                <div class="rvl_right_box">
-                    <dd><span>附</span><span>单</span>据</dd><span class="list_count"></span>
-                    <span class="list_count_num">张</span>
+                <div className="rvl_right_box">
+                    <dd><span>附</span><span>单</span>据</dd><span className="list_count"></span>
+                    <span className="list_count_num">张</span>
                 </div>
         
             </div>
