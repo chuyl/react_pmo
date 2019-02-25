910e35227ea921a2f37915e77dfd927695130bef
liuxuesong930721 <liuxuesong930721@163.com>
2019-02-25 17:12:13 +0800

    clazz_manage

--- src/views/components/watch/TitleLeftCard.js
+++ src/views/components/watch/TitleLeftCard.js
@@ -10,7 +10,7 @@
             return (
                 <div className="card_left card_title_left">
                     {/* <label>{labelValue}</label> */}
-                   <p>{message?message:defaultValue}</p>
+                   <p title={message?message:defaultValue}>{message?message:defaultValue}</p>
                     {/* <span className="text_field_remind"></span> */}
               </div>
             )
