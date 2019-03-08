f38fb46bb284099be5339948cddc73ef44470549
liuxuesong930721 <liuxuesong930721@163.com>
2019-03-08 12:59:48 +0800

    course_manage

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # init
-# scp -r ./build root@aliyun:/var/www/html/erp_app
+ scp -r ./build root@aliyun:/var/www/html/erp_app
  scp -r ./build/static/js/* root@aliyun:/var/www/html/erp_app/build/static/js/
  scp -r ./build/static/css/* root@aliyun:/var/www/html/erp_app/build/static/css/
-# scp -r ./build/static/media/* root@aliyun:/var/www/html/erp_app/build/static/media/
-# scp ./build/index.html root@aliyun:/var/www/html/erp_app/build/
+ scp -r ./build/static/media/* root@aliyun:/var/www/html/erp_app/build/static/media/
+ scp ./build/index.html root@aliyun:/var/www/html/erp_app/build/
